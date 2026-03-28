d699cde5a6c7874065e47c61c5b881b0432fabba
Aye Chanaung <ayechan@example.com>
2026-03-28 17:35:39 +0630

    update icons

---
+++ src/components/CTA.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function CTA() {
+  return (
+    <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start">
+      <a
+        href="https://t.me/lWJXmj8pC7o1YWY1"
+        target="_blank"
+        className="btn-primary"
+      >
+        Join Telegram
+      </a>
+      <a
+        href="#courses"
+        className="btn-secondary"
+      >
+        Browse Courses
+      </a>
+    </div>
+  );
+}
